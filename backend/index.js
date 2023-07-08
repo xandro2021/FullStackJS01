2b69529ca1531386e8eefc9f8ba23c687b94a112
xandro2021 <xandrocastro2021@gmail.com>
2023-07-07 19:15:50 -0600

    Instalar nodemon

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log('Desde NodeJS');
+import express from "express";
+
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('Hola Mundo');
+});
+
+app.listen(4000, () => {
+  console.log('Servidor Funcionando en el puerto 4000');
+});

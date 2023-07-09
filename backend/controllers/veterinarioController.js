aefd265a189c4d433adc773ba02c432b3e75027a
xandro2021 <xandrocastro2021@gmail.com>
2023-07-09 11:26:50 -0600

    usar controladores para asociar funciones a las routes

---
+++ backend/controllers/veterinarioController.js
@@ -0,0 +1,13 @@
+
+const registrar = (req, res) => {
+  res.send('Desde API/VETERINARIOS');
+};
+
+const perfil = (req, res) => {
+  res.send('Desde API/VETERINARIOS/PERFIL');
+};
+
+export {
+  registrar,
+  perfil,
+}

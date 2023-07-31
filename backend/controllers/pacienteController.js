6b75c748619d7ae3c4e14d757a4039469b45b926
xandro2021 <xandrocastro2021@gmail.com>
2023-07-31 14:31:21 -0600

    Finalizar operacion CRUD para pacientes

--- backend/controllers/pacienteController.js
+++ backend/controllers/pacienteController.js
@@ -20,25 +20,67 @@ const obtenerPacientes = async (req, res) => {
   res.json(pacientes);
 };
 
-const obtenerPaciente = async (req, res)  => {
+const obtenerPaciente = async (req, res) => {
   const { id } = req.params;
   const paciente = await Paciente.findById(id);
 
+  if (!paciente) {
+    res.status(404).json({ msg: 'No Encontrado' });
+  }
+
   // Cuando se comparan los _id de mongodb se deben convertir a String
-  if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+  if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
     return res.json({ msg: 'Accion no valida' });
   }
 
-  if(paciente) {
-    res.json(paciente);
+  res.json(paciente);
+}
+
+const actualizarPaciente = async (req, res) => {
+  const { id } = req.params;
+  const paciente = await Paciente.findById(id);
+
+  if (!paciente) {
+    res.status(404).json({ msg: 'No Encontrado' });
+  }
+
+  // La persona que lo va editar es la que lo ha creado
+  if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+    return res.json({ msg: 'Accion no valida' });
+  }
+
+  // Actualizar Paciente || Si no esta el valor en el request entonces se asigna el mismo valor
+  paciente.nombre = req.body.nombre || paciente.nombre;
+
+  try {
+    const pacienteActualizado = await paciente.save();
+    res.json(pacienteActualizado);
+
+  } catch (err) {
+    console.log(err);
   }
 }
 
-const actualizarPaciente = async (req, res)  => {
+const eliminarPaciente = async (req, res) => {
+  const { id } = req.params;
+  const paciente = await Paciente.findById(id);
 
-}
+  if (!paciente) {
+    res.status(404).json({ msg: 'No Encontrado' });
+  }
 
-const eliminarPaciente = async (req, res)  => {
+  // La persona que lo va editar es la que lo ha creado
+  if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+    return res.json({ msg: 'Accion no valida' });
+  }
+
+  try {
+    await paciente.deleteOne();
+    res.json({ msg: 'Paciente Eliminado' });
+
+  } catch (error) {
+    console.log(error);
+  }
 
 }
 

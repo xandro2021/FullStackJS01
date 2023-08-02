fe80d478c4893649c3ecfcfa25cdbc0b146ba349
xandro2021 <xandrocastro2021@gmail.com>
2023-08-01 18:58:10 -0600

    Login Sencillo Usando React y TailwindCSS (prototipo)

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,7 +1,50 @@
 const Login = () => {
   return (
     <>
-      <h2>Login</h2>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Inicia sesion y Administra tus
+          <span className="text-black"> pacientes</span>
+        </h1>
+      </div>
+
+      <div>
+        <form>
+          <div className="my-5">
+            <label
+              className="uppercase text-gray-600 block text-xl font-bold"
+              htmlFor=""
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Email de Registro"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+
+          <div className="my-5">
+            <label
+              className="uppercase text-gray-600 block text-xl font-bold"
+              htmlFor=""
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Tu Password"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Iniciar Session"
+            className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+          />
+        </form>
+      </div>
     </>
   );
 };

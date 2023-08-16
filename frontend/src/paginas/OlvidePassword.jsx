17ee4769c66d44d43123fb8fa9cab61e7407916f
xandro2021 <xandrocastro2021@gmail.com>
2023-08-15 19:16:30 -0600

    Crear componente react para OlvidePassword

--- frontend/src/paginas/OlvidePassword.jsx
+++ frontend/src/paginas/OlvidePassword.jsx
@@ -1,6 +1,38 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Alerta from '../components/Alerta';
+import clienteAxios from '../config/axios';
 
 const OlvidePassword = () => {
+  const [email, setEmail] = useState('');
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Validacion
+    if (email === '' || email.length < 6) {
+      setAlerta({ msg: 'El email es obligatorio', error: true });
+      return;
+    }
+
+    try {
+      const { data } = await clienteAxios.post('/veterinarios/olvide-password', { email });
+
+      setAlerta({
+        msg: data.msg
+      });
+
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <div>
@@ -12,7 +44,14 @@ const OlvidePassword = () => {
 
 
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-        <form>
+
+        {msg && <Alerta
+          alerta={alerta}
+        />}
+
+        <form
+          onSubmit={handleSubmit}
+        >
           <div className="my-5">
             <label
               className="uppercase text-gray-600 block text-xl font-bold"
@@ -24,6 +63,8 @@ const OlvidePassword = () => {
               type="email"
               placeholder="Email de Registro"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
             />
           </div>
 

f16168a616ef1dc99008bfe7673ae46cf2bef0b7
xandro2021 <xandrocastro2021@gmail.com>
2023-08-15 19:17:29 -0600

    Crear helper para recuperar email en el controler

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -2,6 +2,7 @@ import Veterinario from '../models/Veterinario.js';
 import generarJWT from '../helpers/generarJWT.js';
 import generarId from '../helpers/generarId.js';
 import emailRegistro from '../helpers/emailRegistro.js';
+import emailOlvidePassword from '../helpers/emailOlvidePassword.js';
 
 const registrar = async(req, res) => {
   // const { email, password, nombre } = req.body;
@@ -103,7 +104,14 @@ const olvidePassword = async (req, res) => {
   try {
     existeVeterinario.token = generarId();
     await existeVeterinario.save();
-    console.log(existeVeterinario);
+
+    // Enviar email con Instrucciones
+    emailOlvidePassword({
+      email,
+      nombre: existeVeterinario.nombre,
+      token: existeVeterinario.token,
+    })
+
     res.json({ msg: 'Hemos enviado un email con las instrucciones' });
   } catch (error) {
     console.log('error');

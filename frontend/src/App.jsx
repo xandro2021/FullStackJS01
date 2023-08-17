835ffadf054943ae046d3d5ea0e624e2029c9725
xandro2021 <xandrocastro2021@gmail.com>
2023-08-17 16:45:10 -0600

    crear State global para el inicio de usuario

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,22 +6,27 @@ import OlvidePassword from './paginas/OlvidePassword';
 import ConfirmarCuenta from './paginas/ConfirmarCuenta';
 import NuevoPassword from './paginas/NuevoPassword';
 
+// Estado global
+import { AuthProvider } from './context/AuthProvider';
+
 function App() {
   console.log(import.meta.env.VITE_BACKEND_URL);
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <AuthLayout /> }>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
 
-          <Route index element={ <Login /> } />
-          <Route path="registrar" element={ <Registrar /> } />
-          <Route path="olvide-password" element={ <OlvidePassword /> } />
-          <Route path="olvide-password/:token" element={ <NuevoPassword /> } />
-          <Route path="confirmar/:id" element={ <ConfirmarCuenta /> } />
+            <Route index element={<Login />} />
+            <Route path="registrar" element={<Registrar />} />
+            <Route path="olvide-password" element={<OlvidePassword />} />
+            <Route path="olvide-password/:token" element={<NuevoPassword />} />
+            <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
 
-        </Route>
-      </Routes>
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }

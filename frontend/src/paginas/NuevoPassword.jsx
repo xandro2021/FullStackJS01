eb3cac3a5acf02a6648a384c524bcd41fe5e9099
xandro2021 <xandrocastro2021@gmail.com>
2023-08-16 20:46:41 -0600

    Reducir import

--- frontend/src/paginas/NuevoPassword.jsx
+++ frontend/src/paginas/NuevoPassword.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Alerta from '../components/Alerta';
 import clienteAxios from '../config/axios';
 

b17b5871da0c538da629854fcaad6138fbadcedb
xandro2021 <xandrocastro2021@gmail.com>
2023-08-17 16:47:15 -0600

    Crear pagina de inicio y verificar el usuario

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,6 +1,48 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+import Alerta from '../components/Alerta';
+import clienteAxios from '../config/axios';
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alerta, setAlerta] = useState({});
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    if ([email, password].includes('')) {
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true,
+      });
+
+      return;
+    }
+
+    try {
+      const { data } = await clienteAxios.post('/veterinarios/login', { email, password });
+      localStorage.setItem('token', data.token);
+
+      // Redirecciona al Usuario
+      navigate('/admin');
+
+    }
+    catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <div>
@@ -11,7 +53,12 @@ const Login = () => {
       </div>
 
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-        <form>
+
+        {msg && <Alerta
+          alerta={alerta}
+        />}
+
+        <form onSubmit={handleSubmit} >
           <div className="my-5">
             <label
               className="uppercase text-gray-600 block text-xl font-bold"
@@ -23,6 +70,8 @@ const Login = () => {
               type="email"
               placeholder="Email de Registro"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
             />
           </div>
 
@@ -37,6 +86,8 @@ const Login = () => {
               type="password"
               placeholder="Tu Password"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
             />
           </div>
 

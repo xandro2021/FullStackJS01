d5bfd29c48b4451ff786b686172cc34e3182718f
xandro2021 <xandrocastro2021@gmail.com>
2023-08-18 20:11:22 -0600

    Crear Footer y Header para rutas protegidas

--- frontend/src/layout/RutaProtegida.jsx
+++ frontend/src/layout/RutaProtegida.jsx
@@ -1,4 +1,6 @@
 import { Outlet, Navigate } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import useAuth from '../hooks/useAuth';
 
 const RutaProtegida = () => {
@@ -8,10 +10,16 @@ const RutaProtegida = () => {
 
   return (
     <>
-      <h1>Esta es una Ruta Protegida</h1>
+      <Header />
 
       {/* Si no hay authenticacion entonces redireciono a registro */}
-      {auth?._id ? <Outlet /> : <Navigate to="/" />}
+      {auth?._id ? (
+        <main className="container mx-auto mt-10">
+          <Outlet />
+        </main>
+      ) : <Navigate to="/" />}
+
+      <Footer />
     </>
   );
 };

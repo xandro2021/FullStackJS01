3fcab1d0f06ca7d8b10c1d7c8477c32a2180115b
xandro2021 <xandrocastro2021@gmail.com>
2023-08-22 14:48:42 -0600

    Guardar el paciente registrado en la base de datos y State

--- frontend/src/context/PacientesProvider.jsx
+++ frontend/src/context/PacientesProvider.jsx
@@ -4,11 +4,35 @@ import clienteAxios from '../config/axios';
 const PacientesContext = createContext();
 
 export const PacientesProvider = ({ children }) => {
+  const [pacientes, setPacientes] = useState([]);
+
+  const guardarPaciente = async (paciente) => {
+    try {
+      const token = localStorage.getItem('token');
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        }
+      }
+
+      const { data } = await clienteAxios.post('/pacientes', paciente, config);
+
+      const { createdAt, updatedAt, __v, ...pacienteAlmacenado } = data;
+
+      setPacientes([pacienteAlmacenado, ...pacientes]);
+    }
+    catch(error) {
+      console.log(error.response.data.msg);
+    }
+  };
 
   return (
     <PacientesContext.Provider
       value={{
-
+        pacientes,
+        guardarPaciente,
       }}
     >
       {children}

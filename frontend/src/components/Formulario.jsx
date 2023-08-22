50d4b8c5a3bd09d7530457f8a63f7001c4405a8b
xandro2021 <xandrocastro2021@gmail.com>
2023-08-21 20:19:21 -0600

    Crear Formulario y validar basicamente

--- frontend/src/components/Formulario.jsx
+++ frontend/src/components/Formulario.jsx
@@ -1,4 +1,32 @@
+import { useState } from 'react';
+import Alerta from './Alerta';
+
 const Formulario = () => {
+  const [nombre, setNombre] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fecha, setFecha] = useState(Date.now());
+  const [sintomas, setSintomas] = useState('');
+
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // Validar el Formulario
+    if ([nombre, propietario, email, fecha, sintomas].includes('')) {
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+      });
+
+      return;
+    }
+
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <p className="text-lg text-center mb-10">
@@ -6,19 +34,24 @@ const Formulario = () => {
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white py-10 px-5 mb-10 lg:mb-0 shadow-md rounded-md">
+      <form
+        className="bg-white py-10 px-5 mb-10 lg:mb-5 shadow-md rounded-md"
+        onSubmit={handleSubmit}
+      >
 
         <div className="mb-5">
           <label
-            htmlFor="mascota"
+            htmlFor="nombre"
             className="text-gray-700 uppercase font-bold"
           > Nombre Mascota </label>
 
           <input
-            id="mascota"
+            id="nombre"
             type="text"
             placeholder="Nombre de la Mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={nombre}
+            onChange={e => setNombre(e.target.value)}
           />
         </div>
 
@@ -33,6 +66,8 @@ const Formulario = () => {
             type="text"
             placeholder="Nombre del Propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={propietario}
+            onChange={e => setPropietario(e.target.value)}
           />
         </div>
 
@@ -47,6 +82,8 @@ const Formulario = () => {
             type="email"
             placeholder="Email del Propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
         </div>
 
@@ -60,6 +97,8 @@ const Formulario = () => {
             id="fecha"
             type="date"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={fecha}
+            onChange={e => setFecha(e.target.value)}
           />
         </div>
 
@@ -73,6 +112,8 @@ const Formulario = () => {
             id="sintomas"
             placeholder="Describe los Sintomas"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={sintomas}
+            onChange={e => setSintomas(e.target.value)}
           />
         </div>
 
@@ -83,6 +124,13 @@ const Formulario = () => {
         />
 
       </form>
+
+      {msg &&
+        <Alerta
+          alerta={alerta}
+        />
+      }
+
     </>
   );
 };

e2fabffae52f6c6f16dc753f244eddfed8580461
xandro2021 <xandrocastro2021@gmail.com>
2023-08-22 21:25:34 -0600

    Mostrar el componente de los pacientes asignados

--- frontend/src/components/ListadoPacientes.jsx
+++ frontend/src/components/ListadoPacientes.jsx
@@ -1,8 +1,8 @@
 import usePacientes from '../hooks/usePacientes';
+import Paciente from './Paciente';
 
 const ListadoPacientes = () => {
   const { pacientes } = usePacientes();
-  console.log(pacientes);
 
   return (
     <>
@@ -15,6 +15,13 @@ const ListadoPacientes = () => {
               Administra a tus {''}
               <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
             </p>
+
+            {pacientes.map(paciente => (
+              <Paciente
+                key={paciente._id}
+                paciente={paciente}
+              />
+            ))}
           </>
         ) :
         (

3dea36eddfabe8f0e33abf617ef74933c102b6d6
xandro2021 <xandrocastro2021@gmail.com>
2023-08-28 18:09:25 -0600

    Formulario para cambiar password

--- frontend/src/paginas/CambiarPassword.jsx
+++ frontend/src/paginas/CambiarPassword.jsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import AdminNav from '../components/AdminNav';
+import Alerta from '../components/Alerta'
 
 const CambiarPassword = () => {
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <AdminNav />
@@ -12,6 +22,48 @@ const CambiarPassword = () => {
         <span className="text-indigo-600 font-bold">Password Aqui</span>
       </p>
 
+      <div className="flex justify-center">
+        <div className="w-full md:w-1/2 bg-white shadow rounded-lg p-5">
+
+          {msg && <Alerta
+            alerta={alerta}
+          />}
+
+          <form
+            onSubmit={handleSubmit}
+          >
+
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600" htmlFor="">Password Actual</label>
+
+              <input
+                type="text"
+                className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                name="nombre"
+                placeholder="Escribe tu password actual"
+              />
+            </div>
+
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600" htmlFor="">Nuevo Password</label>
+
+              <input
+                type="text"
+                className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                name="nombre"
+                placeholder="Escribe tu nuevo password"
+              />
+            </div>
+
+            <input
+              type="submit"
+              value="Actualizar Password"
+              className="bg-indigo-700 px-10 py-3 font-bold text-white rounded-lg uppercase w-full mt-5 hover:cursor-pointer hover:bg-indigo-800"
+            />
+
+          </form>
+        </div>
+      </div>
     </>
   );
 };
